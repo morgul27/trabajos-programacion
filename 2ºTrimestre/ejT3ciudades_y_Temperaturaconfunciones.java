c76b500ca40c72af4fd3182af103a4f883b95c8a
morgul27 <joseminanca2000@gmail.com>
2023-01-14 18:12:11 +0100

    a

---
+++ 2ºTrimestre/ejT3ciudades_y_Temperaturaconfunciones.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ejT3ciudades_y_Temperaturaconfunciones {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> ciudad = new ArrayList<String>();
+        ArrayList<Double> temp = new ArrayList<Double>();
+        ArrayList<String> c_mayorLista = new ArrayList<String>();
+        ArrayList<String> c_menorLista = new ArrayList<String>();
+        String F;
+        Double T;
+        Double mayor = 0.0;
+        Double menor = 10000.0;
+        Double suma = 0.0;
+        int c = 0;
+        int posicion;
+        double media;
+        boolean m = false;
+
+        do {
+            do {
+                System.out.println("Pon el nombre de la ciudad o FIN para terminar");
+                F = sc.next();
+
+                for (int i = 0; i < ciudad.size(); i++) {
+                    if (ciudad.contains(F) == true)
+                        m = ciudad.contains(F);
+                    else
+                        m = false;
+                }
+
+            } while (m == true);
+
+            if (F.equals("FIN") || F.equals("fin"))
+                break;
+            ciudad.add(F);
+            do {
+                System.out.println("Di la temperatura de esa ciudad");
+                T = sc.nextDouble();
+            } while (T < 0 || T > 50);
+            temp.add(T);
+
+            mayor = (T > mayor) ? T : mayor;
+            if (T == mayor)
+                c_mayorLista.add(F);
+            menor = (T < menor) ? T : menor;
+            if (T == menor)
+                c_mayorLista.add(F);
+            suma += T;
+            c++;
+
+        } while (!F.equals("FIN") || !F.equals("fin"));
+
+        System.out.println();
+        media = suma / c;
+        System.out.println("La temperatura mayor es " + mayor + "ºC la temperatura menor es " + menor
+                + "ºC y la media es " + media + "ºC");
+
+        posicion = temp.indexOf(mayor);
+        System.out.println("La ciudad con mayor temperatura es " + ciudad.get(posicion));
+        posicion = temp.indexOf(menor);
+        System.out.println("La ciudad con menor temperatura es " + ciudad.get(posicion));
+
+        System.out.println();
+        System.out.println("la ciudades más grande que la media son:");
+        for (int i = 0; i < temp.size(); i++) {
+            if (temp.get(i) > media)
+                System.out.println(ciudad.get(i));
+        }
+    }
+
+    // funcion para ver si se repite ciudad
+    public static boolean repetir(ArrayList<String> ciudad, String F) {
+        boolean m = false;
+        for (int i = 0; i < ciudad.size(); i++) {
+            if (ciudad.contains(F) == true)
+                m = ciudad.contains(F);
+            else
+                m = false;
+        }
+        System.out.println(m);
+        return m;
+    }
+
+    // funcion para rellenar las arraylist de las ciudades con mayor o menor
+    // temperatura
+    public static ArrayList<String> rellenar(ArrayList<String> ciu_lista, Double T, String F, Double grados) {
+
+        grados = (T > grados) ? T : grados;
+        if (T == grados)
+            ciu_lista.add(F);
+        return ciu_lista;
+    }
+}

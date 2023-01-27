66215dea86c4c3800ef11d8fb53c783bbcd1f097
morgul27 <joseminanca2000@gmail.com>
2023-01-27 14:03:31 +0100

    V2.2

--- 2ºTrimestre/juegoAzar.java
+++ 2ºTrimestre/juegoAzar.java
@@ -4,100 +4,129 @@ import java.util.ArrayList;
 public class juegoAzar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> numApostado = new ArrayList<Integer>();
-        Double saldo = 0.00;
+        ArrayList<Integer> numApostadoLo = new ArrayList<Integer>();
+        ArrayList<Integer> numApostadoEuro = new ArrayList<Integer>();
+        Double saldo = 50.00;
         int x;
-
-        System.out.println("");
-        System.out.println("1. Loteria");
-        System.out.println("2. Euromillon");
-        System.out.println("3. Meter saldo");
-        System.out.println("4. Sacar saldo");
-        System.out.println("5. Ver resultados");
+        boolean salirMenu = false;
 
         // Menu para jugar
-        x = sc.nextInt();
-        switch (x) {
-            case 1:
-                loteria(saldo, numApostado);
-                break;
-            case 2:
-                System.out.println("euromillon");
-                break;
-            case 3:
-                saldo = metersaldo(saldo);
-                break;
-            case 4:
-                saldo = sacarsaldo(saldo);
-                break;
-            case 5:
-                break;
-            default:
-                System.out.println("Se ha equivocado");
+        do {
+            System.out.println("");
+            System.out.println("1. Loteria");
+            System.out.println("2. Euromillon");
+            System.out.println("3. Meter saldo");
+            System.out.println("4. Sacar saldo");
+            System.out.println("5. Ver resultados");
+            System.out.println("9. Salir");
 
-        }
+            x = sc.nextInt();
+            switch (x) {
+                case 1:
+                    saldo = loteria(saldo, numApostadoLo);
+                    break;
+                case 2:
+                    saldo = Euromillon(saldo, numApostadoEuro);
+                    break;
+                case 3:
+                    saldo = saldo(saldo, x);
+                    break;
+                case 4:
+                    saldo = saldo(saldo, x);
+                    break;
+                case 5:
+                    break;
+                case 9:
+                    salirMenu = true;
+                    break;
+                default:
+                    System.out.println("Se ha equivocado");
 
-        premios(numApostado);
+            }
+        } while (!salirMenu);
+
+        premios(numApostadoLo, numApostadoEuro);
 
     }
 
-    // funcion loteria
-    public static ArrayList<Integer> loteria(Double saldo, ArrayList<Integer> numApostado) {
+    // funcion LOTERIA
+    public static double loteria(Double saldo, ArrayList<Integer> numApostadoLo) {
         Scanner sc = new Scanner(System.in);
         int opcion;
         String aceptar;
-        int x;
-        int num = 0;
 
         if (saldo >= 3) {
             do {
-                System.out.println("¿Quieres meter numero(0) o que sea aleatorio?(1)");
-                opcion = sc.nextInt();
-                if (opcion == 1)
-                    numApostado.add(aleatorio(0, 99999));
-                else if (opcion == 0) {
-                    System.out.println("mete numero");
-                    numApostado.add(sc.nextInt());
-                }
-
-                saldo -= 3;
-                System.out.println("su numero es: " + numApostado);
+                pedirnumero(numApostadoLo, 0, 99999);
 
                 System.out.println("Quieres pedir otro? Di si para aceptar, tienes " + saldo + " euros");
-                aceptar = sc.nextLine();
+                aceptar = sc.next();
                 System.out.println("");
 
             } while (aceptar.equals("si") && saldo >= 3);
         }
+        return saldo;
+    }
+
+    // funcion EUROMILLON
+    public static double Euromillon(Double saldo, ArrayList<Integer> numApostadoEuro) {
+        Scanner sc = new Scanner(System.in);
+        String aceptar;
+
+        System.out.println("Estas en el EUROMILLON");
+        do {
+
+            for (int c = 0; c < 5; c++) {
+                pedirnumero(numApostadoEuro, 1, 49);
+            }
+            saldo -= 2.5;
+
+            System.out.println("Quieres pedir otro? Di si para aceptar, tienes " + saldo + " euros");
+            aceptar = sc.nextLine();
+            System.out.println("");
+
+        } while (aceptar.equals("si") && saldo >= 2.5);
+        return saldo;
+    }
+
+    // funcion de numero ver arriba loteria
+    // funcion para meter numero dentro de un juego
+    public static ArrayList<Integer> pedirnumero(ArrayList<Integer> numApostado, int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        String aceptar = " ";
+
+        System.out.println("¿Quieres meter numero(0) o que sea aleatorio?(1)");
+        opcion = sc.nextInt();
+        if (opcion == 1) {
+            numApostado.add(aleatorio(min, max));
+            System.out.println("Su numero es: " + numApostado);
+        } else if (opcion == 0) {
+            System.out.println("mete numero");
+            numApostado.add(sc.nextInt());
+            System.out.println("Su numero es: " + numApostado);
+        }
+
         return numApostado;
-
     }
 
-    // funcion euromillon
-    public static void euromillon() {
-
-    }
-
-    // funcion para el meter saldo
-    public static double metersaldo(Double saldo) {
+    // funcion para el meter o sacar saldo
+    public static double saldo(Double saldo, int marcar) {
         Scanner sc = new Scanner(System.in);
-        double suma = 0;
+        double pedir = 0;
 
-        System.out.println("Introduzca su saldo");
-        saldo = sc.nextDouble();
-        suma += saldo;
-        return suma;
-    }
+        if (marcar == 3) {
+            System.out.println("Introduzca el dinero que quiera meter");
+            pedir = sc.nextDouble();
+            pedir += saldo;
+        } else if (marcar == 4) {
+            System.out.println("Introduzca el dinero que quiera sacar");
+            pedir = sc.nextDouble();
+            pedir -= saldo;
+        }
 
-    // funcion para sacar el saldo
-    public static double sacarsaldo(Double saldo) {
-        Scanner sc = new Scanner(System.in);
-        double resta = 0;
-
-        System.out.println("Introduzca su saldo");
-        saldo = sc.nextDouble();
-        resta = saldo - resta;
-        return resta;
+        System.out.println("Su saldo es " + pedir);
+        return pedir;
     }
 
     // funcion aleatorio
@@ -113,10 +142,11 @@ public class juegoAzar {
     }
 
     // funcion para numeros premiados
-    public static void premios(ArrayList<Integer> numApostado) {
-        int num;
-        num = aleatorio(0, 9999);
-        System.out.println("El numero premiado de la loteria es " + num);
-        System.out.println("Su numero es " + numApostado);
+    public static void premios(ArrayList<Integer> numApostadoLo, ArrayList<Integer> numApostadoEuro) {
+        int premiado;
+        premiado = aleatorio(0, 9999);
+        System.out.println("El numero premiado de la loteria es " + premiado);
+        System.out.println("Su numero es " + numApostadoLo);
+        System.out.println("Sus numeros de euromillon son " + numApostadoEuro);
     }
 }

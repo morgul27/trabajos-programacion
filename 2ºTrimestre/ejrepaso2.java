f46bd1e26afc5bc2a8559bcc13c6bffc88f1d625
morgul27 <joseminanca2000@gmail.com>
2023-02-07 14:30:15 +0100

    a

--- 2ºTrimestre/ejrepaso2.java
+++ 2ºTrimestre/ejrepaso2.java
@@ -8,17 +8,25 @@ import java.util.ArrayList;
 
 public class ejrepaso2 {
     public static void main(String[] args) {
-        ArrayList<Integer> temperatura = new ArrayList<Integer>();
-        String ciudades[] = { "Sevilla", "Cadiz", "Jaen", "Cordoba" };
+        ArrayList<String> idCiudades = new ArrayList<String>();
+        idCiudades.add("Sevilla");
+        idCiudades.add("Cadiz");
+        idCiudades.add("Jaen");
+
+        int fecha = 0, temp_max = 0, temp_min = 0, hora_max = 0, hora_min = 0;
         int ran = 0;
         String ciudad;
 
-        // elegir 3 ciudades
-        for (int i = 0; i < 3; i++) {
-            ran = aleatorio(0, 3);
-            ciudad = ciudades[ran];
-        }
-        System.out.println(temperatura);
+        // orden de la extructura de datos
+        // { idCiudades, fecha, temp_max, temp_min, hora_max, hora_min }
+        Object datos[][] = {
+                { idCiudades, fecha, temp_max, temp_min, hora_max, hora_min },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+        };
+
     }
 
     // funcion aleatorio
@@ -33,10 +41,37 @@ public class ejrepaso2 {
         return ran;
     }
 
+    // funcion for
+    public static void parafor(int a, int b, int idCiudad[]) {
+        int min;
+        int max;
+        int random = 5;
+        int TDias = 5;
+        int Datos[][];
+        Datos = new int[5][5];
+
+        for (int i = 0; i < idCiudad.length; i++) {
+            for (int j = 0; j < TDias; j++) {
+                Datos[i * TDias + j][0] = i; // ciudad
+                Datos[i * TDias + j][1] = j; // fecha
+                Datos[i * TDias + j][2] = random; // temp max, hacer un random
+                Datos[i * TDias + j][3] = random; // temp min, hacer un random
+            }
+        }
+    }
+
     // anotaciones
 
     // hacer un solo random para las horas, se calcula solo los segundos y luego se
-    // saca las horas
+    // saca las horas, 24x60x60 son los segundos en un dia
 
     // arraylist Object
+
+    // se supone que dos for
+
+    // matriz ocupa tanta ciudades por dias
+
+    // calcular TDias con las funciones de fechas
+
+    // i*TDias+j
 }

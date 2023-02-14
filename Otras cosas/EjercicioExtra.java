a51a86748a81fd5ded632de62afc7e85e061e1ab
morgul27 <joseminanca2000@gmail.com>
2023-02-14 14:17:00 +0100

    14/02/2023

---
+++ Otras cosas/EjercicioExtra.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.text.NumberFormat;
+import java.text.*;
+import java.util.Locale;
+
+public class EjercicioExtra {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}

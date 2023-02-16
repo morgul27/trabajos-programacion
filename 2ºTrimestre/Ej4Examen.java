fa84418bead22578a2dfb8ffc313293e4bf11628
morgul27 <joseminanca2000@gmail.com>
2023-02-16 10:12:45 +0100

    16/02/2023

--- 2ºTrimestre/Ej4Examen.java
+++ 2ºTrimestre/Ej4Examen.java
@@ -10,22 +10,32 @@ public class Ej4Examen {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> numApostado = new ArrayList<Integer>();
+        Integer T = new Integer(0);
+        // en la T(tirada) se guardan el orden por asi decirlo y d1 y d2 son los numeros
+        // que salen
         int d1;
         int d2;
         int aux;
         Object matriz[][] = {
-
                 { 0 }, // 6-6
+                // 1-1
                 { 0, 0 }, // 5-5 5-6
+                // 2-1 2-2
                 { 0, 0, 0 }, // 4-4 4-5 4-6
+                // 3-1 3-2 3-3
                 { 0, 0, 0, 0 }, // 3-3 3-4 3-5 3-6
+                // 4-1 4-2 4-3 4-4
                 { 0, 0, 0, 0, 0 }, // 2-2 2-3 2-4 2-5 2-6
+                // 5-1 5-2 5-3 5-4 5-5
                 { 0, 0, 0, 0, 0, 0 }, // 1-1 1-2 1-3 1-4 1-5 1-6
+                // 6-1 6-2 6-3 6-4 6-5 6-6
         };
+        ArrayList<Integer> m[][] = new ArrayList[6][];
 
         for (int i = 0; i < matriz.length; i++) {
+            m[i] = new ArrayList[i + 1];
             for (int j = 0; j <= i; j++) {
-                matriz[i][j] = new ArrayList();
+                m[i][j] = new ArrayList<Integer>();
             }
         }
 
@@ -33,15 +43,16 @@ public class Ej4Examen {
             for (int j = 0; j <= i; j++) {
                 d1 = aleatorio();
                 d2 = aleatorio();
+                T = Integer.valueOf(i);
+
                 if (d1 < d2) { // d1 tiene que ser mayor que d1 siempre porque es la filas
                     aux = d1;
                     d1 = d2;
                     d2 = aux;
                 }
                 System.out.println("d1 " + d1 + " d2 " + d2);
-                matriz[d1 - 1][d2 - 1] = 1;
+                m[d1][d2].add(T);
 
-                // matriz[d1 - 1][d2 - 1] = add(d1+""+d2);
             }
         }
 
@@ -50,7 +61,7 @@ public class Ej4Examen {
 
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print(matriz[i][j] + " ");
+                System.out.print(m[i][j]);
             }
             System.out.println(" ");
         }
@@ -61,7 +72,7 @@ public class Ej4Examen {
     public static int aleatorio() {
         int ran;
 
-        ran = (int) (Math.floor((6) * Math.random()) + 1);
+        ran = (int) (Math.floor((6) * Math.random()));
         return ran;
     }
 

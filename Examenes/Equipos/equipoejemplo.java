34e82cb46853b826efb28546b54b2f5556bbaff1
morgul27 <joseminanca2000@gmail.com>
2023-03-21 11:57:22 +0100

    21/03/2023

--- Examenes/Equipos/equipoejemplo.java
+++ Examenes/Equipos/equipoejemplo.java
@@ -7,14 +7,38 @@ public class equipoejemplo {
     public static void main(String[] args) {
         equipo e1 = new equipo();
         equipo e2 = new equipo();
+        equipo e3 = new equipo();
+        equipo e4 = new equipo();
+        equipo e5 = new equipo();
+        equipo e6 = new equipo();
+        equipo e7 = new equipo();
+        equipo e8 = new equipo();
 
-        equipo aray[] = { e1, e2 };
+        equipo aray[] = { e1, e2, e3, e4, e5, e6, e7, e8 };
 
         e1.setRanking(15);
         e1.setNombre("Betis");
 
         e2.setRanking(45);
         e2.setNombre("Sevilla");
+
+        e3.setRanking(35);
+        e3.setNombre("Barcelona");
+
+        e4.setRanking(25);
+        e4.setNombre("Madrid");
+
+        e5.setRanking(50);
+        e5.setNombre("Cadiz");
+
+        e6.setRanking(6);
+        e6.setNombre("");
+
+        e7.setRanking(45);
+        e7.setNombre("Sevilla");
+
+        e8.setRanking(45);
+        e8.setNombre("Sevilla");
 
         mostrar(aray);
         System.out.println();

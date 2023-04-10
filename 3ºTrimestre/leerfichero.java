7252ee2a3135400008cf19d166183e2400940ceb
morgul27 <joseminanca2000@gmail.com>
2023-04-10 12:45:43 +0200

    10-04-2023

---
+++ 3ºTrimestre/leerfichero.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class leerfichero {
+    public static void main(String[] args) throws IOException {
+        File fichero = new File("C:/Programacion_c/ej1.txt");
+        FileReader fic = new FileReader(fichero);
+        int i;
+
+        while ((i = fic.read()) != -1)
+            System.out.println(i);
+        fic.close();
+
+    }
+
+}

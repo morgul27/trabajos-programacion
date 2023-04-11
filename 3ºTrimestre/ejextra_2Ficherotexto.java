cebae011c461574c7075ecef9bfb3a78428700dc
morgul27 <joseminanca2000@gmail.com>
2023-04-11 14:33:03 +0200

    a

--- 3ºTrimestre/ejextra_2Ficherotexto.java
+++ 3ºTrimestre/ejextra_2Ficherotexto.java
@@ -2,6 +2,27 @@ import java.io.*;
 
 public class ejextra_2Ficherotexto {
     public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader("Lista de caracteres.txt");
+            FileWriter fw = new FileWriter("fichero1.txt");
+            FileWriter fw2 = new FileWriter("fichero2.txt");
 
+            int valor = fr.read();
+            while (valor != -1) {
+
+                System.out.println((char) valor);
+                fw.write((char) valor);
+                fw.flush();
+                fw2.write((char) valor);
+                valor = fr.read();
+            }
+            // Cerrar el stream
+            fw.close();
+            fr.close();
+            fw2.close();
+
+        } catch (IOException e) {
+            System.out.println("Error E/S: " + e);
+        }
     }
 }

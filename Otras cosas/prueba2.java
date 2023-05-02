e914219aca0c247437eecc275bf683c53ed0ed99
morgul27 <joseminanca2000@gmail.com>
2023-05-02 18:32:19 +0200

    F

--- Otras cosas/prueba2.java
+++ Otras cosas/prueba2.java
@@ -2,14 +2,33 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class prueba2 {
+
     public static void main(String[] args) {
-        // random
-        // Método para GENERAR UN ENTERO ALEAORIO DENTRO DE UN RANGO
-        int menor = 3;
-        int mayor = 8;
-        int n = (int) Math.floor((mayor - menor + 1) * Math.random()) + menor;
-        System.out.println(n);
+        String db_ = "Prueba";
+        String login_ = "root";
+        String password_ = "";
+        String url_ = "jdbc:mysql://127.0.0.1/" + db_;
+        Connection connection_;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection_ = DriverManager.getConnection(url_, login_, password_);
+
+            System.out.println("Conexion a base de datos " + db_ + " correcta.");
+            connection_.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

4197676edcaaf2ebf0e29ebb04df1e7e101bf55c
morgul27 <joseminanca2000@gmail.com>
2023-05-18 10:02:54 +0200

    1111

--- Otras cosas/Entorno/calculadora.java
+++ Otras cosas/Entorno/calculadora.java
@@ -9,6 +9,9 @@ public class calculadora {
         int num1;
         int num2;
 
+        // El debug para antes de que se ejecute el punto de ruptura, es decir, el punto
+        // rojo
+        // Ctrl+k+c comentar, Ctrl+k+u quitar comentario
         //
         try {
             System.out.println("Introduce un numero");
@@ -19,15 +22,34 @@ public class calculadora {
             System.out.println("Si quieres sumar(1), restar(2), mult(3), div(4)");
             x = sc.nextInt();
 
-            if (x == 1) {
-                x = Operacion.sumar(num1, num2);
-            } else if (x == 2) {
-                x = Operacion.restar(num1, num2);
-            } else if (x == 3) {
-                x = Operacion.mult(num1, num2);
-            } else {
-                x = Operacion.dividir(num1, num2);
+            // if (x == 1) {
+            // x = Operacion.sumar(num1, num2);
+            // } else if (x == 2) {
+            // x = Operacion.restar(num1, num2);
+            // } else if (x == 3) {
+            // x = Operacion.mult(num1, num2);
+            // } else {
+            // x = Operacion.dividir(num1, num2);
+            // }
+
+            switch (x) {
+                case 1:
+                    x = Operacion.sumar(num1, num2);
+                    System.out.println("eeeeeee");
+                    break;
+                case 2:
+                    x = Operacion.restar(num1, num2);
+                    break;
+                case 3:
+                    x = Operacion.mult(num1, num2);
+                    break;
+                case 4:
+                    x = Operacion.dividir(num1, num2);
+                    break;
+                default:
+                    break;
             }
+
             sc.close();
 
             System.out.println("El resultado de la operacion es " + x);

b2d13cb909e8211252b72aefcbb3907223f99c70
morgul27 <joseminanca2000@gmail.com>
2023-05-23 20:13:25 +0200

    ea

--- 3ºTrimestre/clinicabd/clases/TtosRealizados.java
+++ 3ºTrimestre/clinicabd/clases/TtosRealizados.java
@@ -96,7 +96,7 @@ public class TtosRealizados { // SERVICIOS
         IDLiquidacion = iDLiquidacion;
     }
 
-    //funcion cuando quiera insertar
+    // funcion cuando quiera insertar servicios
     public static void insertar() {
         String db_ = "ClinicaDental";
         String login_ = "root";
@@ -124,22 +124,33 @@ public class TtosRealizados { // SERVICIOS
 
             // necesito insertar los id de todo
             for (int i = 0; i < contador; i++) {
+                System.out.println("Introduce el IDServicio " + (contador + 1));
+                int IDServicio = sc.nextInt();
                 System.out.println("Introduce la fecha de realizacion del servicio (yyyy-mm-dd) numero "
                         + (contador + 1));
-                java.sql.Date fecharea = java.sql.Date.valueOf(sc.nextLine());
-                System.out.println("Introduce precio del servicio " + (contador + 1));
-                String precio = sc.nextLine();
-                System.out.println("Introduce cobro importado " + (contador + 1));
-                String cobro = sc.nextLine();
+                String fecha = sc.nextLine();
                 System.out.println("Introduce id del cliente " + (contador + 1));
+                int iDPaciente = sc.nextInt();
                 System.out.println("Introduce id del profesional " + (contador + 1));
-                System.out.println("Introduce id del tratamiento " + (contador + 1));
+                int idprofesional = sc.nextInt();
+                System.out.println("Introduce precio del servicio " + (contador + 1));
+                Double precio = sc.nextDouble();
+                System.out.println("Introduce lo cobrado " + (contador + 1));
+                Double cobrado = sc.nextDouble();
                 System.out.println("Introduce id de la liquidacion " + (contador + 1));
+                int idliquidacion = sc.nextInt();
+                System.out.println("Introduce el ID del Tratamiento " + (contador + 1));
+                int IDTratamiento = sc.nextInt();
 
                 rs_.moveToInsertRow();
-                rs_.updateDate("Fecha", fecharea);
-                rs_.updateString("Precio", precio);
-                rs_.updateString("Cobro", cobro);
+                rs_.updateInt("IDServicio", IDServicio);
+                rs_.updateString("Fecha", fecha);
+                rs_.updateInt("IDPaciente", iDPaciente);
+                rs_.updateInt("IDProfesional", idprofesional);
+                rs_.updateInt("IDTratamiento", IDTratamiento);
+                rs_.updateDouble("Precio", precio);
+                rs_.updateDouble("Cobro", cobrado);
+                rs_.updateInt("IDLiquidacion", idliquidacion);
 
                 rs_.insertRow();
                 rs_.moveToCurrentRow();
@@ -159,6 +170,9 @@ public class TtosRealizados { // SERVICIOS
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // si necesito algo en date
+        // java.sql.Date fecharea = java.sql.Date.valueOf(sc.nextLine());
 
     }
 
